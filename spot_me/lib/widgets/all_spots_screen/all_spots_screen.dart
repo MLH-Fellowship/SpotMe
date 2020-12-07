541b0d6c73222f2ec9ad964dcc332274466d9fe0
Laurell Seville <laurellseville2000@gmail.com>
2020-12-06 19:33:59 -0500

    added listview

--- spot_me/lib/widgets/all_spots_screen/all_spots_screen.dart
+++ spot_me/lib/widgets/all_spots_screen/all_spots_screen.dart
@@ -1,44 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:spot_me/colors.dart';
-import "spot.dart";
+import 'spot_card.dart';
+import 'spot.dart';
 
 class AllSpotsScreen extends StatelessWidget {
+  List _spots = [
+    Spot("56 Groot Street",
+        "Need help with gardening, landscaping,digging in the ground, planting trees andwatering the plants"),
+    Spot("65 Groot Street",
+        "Need help with gardening, landscaping,digging in the ground, planting trees andwatering the plants"),
+    Spot("78 Groot Street",
+        "Need help with gardening, landscaping,digging in the ground, planting trees andwatering the plants"),
+    Spot("89 Groot Street",
+        "Need help with gardening, landscaping,digging in the ground, planting trees andwatering the plants"),
+    Spot("105 Groot Street",
+        "Need help with gardening, landscaping,digging in the ground, planting trees andwatering the plants"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Hero(
       tag: "need",
       child: Scaffold(
         appBar: AppBar(
-          title: Center(child: Text("All Spots")),
+          title: Text("All Spots"),
           elevation: 0,
-          backgroundColor: AppColors.blueColor,
+          backgroundColor: AppColors.magentaColor,
           brightness: Brightness.dark,
         ),
         body: Container(
-            color: AppColors.blueColor,
+            color: AppColors.magentaColor,
             width: double.infinity,
-            child: Column(
-              children: [
-                Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.only(left: 20),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Where you can help",
-                              style: TextStyle(
-                                  fontSize: 40.0, color: Colors.white)),
-                          SizedBox(height: 10),
-                          Text("10 spots around you",
-                              style: TextStyle(
-                                  color: Colors.grey[300], fontSize: 20)),
-                          SizedBox(height: 30),
-                        ])),
-                Column(
-                  children: [Spot(), Spot()],
-                )
-              ],
-            )),
+            child: Column(children: [
+              Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.only(left: 20),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Where you can help",
+                            style:
+                                TextStyle(fontSize: 40.0, color: Colors.white)),
+                        SizedBox(height: 10),
+                        Text("10 spots around you",
+                            style: TextStyle(
+                                color: Colors.grey[300], fontSize: 20)),
+                        SizedBox(height: 30),
+                      ])),
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: _spots.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SpotCard(
+                            title: _spots[index].title,
+                            description: _spots[index].description);
+                      })),
+            ])),
       ),
     );
   }
--- spot_me/lib/widgets/all_spots_screen/spot.dart
+++ spot_me/lib/widgets/all_spots_screen/spot.dart
@@ -1,26 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:spot_me/colors.dart';
+class Spot {
+  String title;
+  String description;
 
-class Spot extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-          padding: EdgeInsets.all(20),
-          width: MediaQuery.of(context).size.width * 0.90,
-          color: Colors.black,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("56 Groot Street",
-                  style: TextStyle(color: Colors.white, fontSize: 30)),
-              SizedBox(height: 20),
-              Text(
-                  "Need help with gardening, landscaping,digging in the ground, planting trees andwatering the plants",
-                  style: TextStyle(color: Colors.white, fontSize: 20))
-            ],
-          )),
-      SizedBox(height: 20)
-    ]);
-  }
+  Spot(this.title, this.description);
 }
